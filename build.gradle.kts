f603fb861547c7b13f5a93bc0cb5666ebf36711d
Dmitrii Belousov <dbelousov@db-mbp.local>
2022-06-06 21:38:26 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.21"
+    id("org.springframework.boot") version "2.7.0"
+}
+apply(plugin = "io.spring.dependency-management")
+
+group = "io.beautrace"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation(kotlin("test"))
+}
+
+springBoot {
+    mainClass.set("io.beautrace.MainKt")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
